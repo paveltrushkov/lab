f80bfaca098906dc29087a69eeea082705a5c936
paveltrushkov <56478385+paveltrushkov@users.noreply.github.com>
2020-02-17 19:54:41 +0700

    1 лаба 10 вариант
    1 задание вроде правильное, 2 нет, но всё равно загрузил

---
+++ lab1/lab1.1/lab1.1/lab1.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream input("massive.txt");
+	float ch; int count = 0;
+	while (input >> ch)
+		count++;
+	cout << count << endl;
+	int* mass;
+	mass = new int[count];
+	string path = "C:\\Users\\Fabillippa\\source\\repos\\lab1.1\\massive.txt";
+	ifstream fin;
+	fin.open(path);
+	if (!fin.is_open())
+	{
+		cout << "Error";
+	}
+	for (int i = 0; i < count; i++)
+		fin >> mass[i];
+	for (int i = 0; i < count; i++)
+		cout << mass[i] << "\n";
+	return 0;
+}
---
+++ lab1/lab1.2/lab1.2/lab1.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	int a, b, n, y = 0, k, count = 0;
+	int* mass;
+	cout << "Enter the size of massive:";
+	cin >> n;
+	mass = new int[n];
+	cout << "Enter the diapazone:"; cin >> a; cin >> b; cout << endl;
+	for (int i = a; i < b; i++)
+	{
+		mass[i] = (rand() % 10 - 10);
+		cout << mass[i] << "\n";
+		count++;
+	}
+	cout << "k = ";
+	cin >> k;
+	for (int i = a; i < b; i++)
+	{
+		mass[i] = mass[i] * 10 + (-k);
+		cout << mass[i] << "\n";
+	}
+	return 0;
+}
