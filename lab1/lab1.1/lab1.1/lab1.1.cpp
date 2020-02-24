fe4fdcf5c9961c11a5a3e5f31ed2dbe10312c5d7
paveltrushkov <56478385+paveltrushkov@users.noreply.github.com>
2020-02-24 15:10:14 +0700

    1 лаба 10 вариант

--- lab1/lab1.1/lab1.1/lab1.1.cpp
+++ lab1/lab1.1/lab1.1/lab1.1.cpp
@@ -3,25 +3,53 @@
 #include <string>
 using namespace std;
 
+void selectionSort(int* num, int size)
+{
+	int min, temp;
+	for (int i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (num[j] < num[min])
+				min = j;
+		}
+		temp = num[i];
+		num[i] = num[min];
+		num[min] = temp;
+	}
+}
+
 int main()
 {
 	ifstream input("massive.txt");
-	float ch; int count = 0;
-	while (input >> ch)
+	int a, count = 0;
+	while (input >> a)
 		count++;
-	cout << count << endl;
+	cout << "amount of numbers: " << count << endl;
 	int* mass;
 	mass = new int[count];
-	string path = "C:\\Users\\Fabillippa\\source\\repos\\lab1.1\\massive.txt";
+	string path = "C:\\Users\\Fabillippa\\Desktop\\lab1\\lab1.1\\lab1.1\\massive.txt";
 	ifstream fin;
 	fin.open(path);
 	if (!fin.is_open())
 	{
 		cout << "Error";
+		return 0;
 	}
+	int countIndexMas = 0;
 	for (int i = 0; i < count; i++)
-		fin >> mass[i];
-	for (int i = 0; i < count; i++)
+	{
+		int temp;
+		fin >> temp;
+		if (temp < 0)
+		{
+			mass[countIndexMas++] = temp;
+		}
+	}
+	selectionSort(mass, countIndexMas);
+	for (int i = 0; i < countIndexMas; i++)
 		cout << mass[i] << "\n";
+
 	return 0;
 }
--- lab1/lab1.2/lab1.2/lab1.2.cpp
+++ lab1/lab1.2/lab1.2/lab1.2.cpp
@@ -2,28 +2,49 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
+
 using namespace std;
 
 int main()
 {
-	int a, b, n, y = 0, k, count = 0;
+	int a, b, n=0, c, k;
 	int* mass;
-	cout << "Enter the size of massive:";
+	cout << "Enter the size of massive: ";
 	cin >> n;
 	mass = new int[n];
-	cout << "Enter the diapazone:"; cin >> a; cin >> b; cout << endl;
-	for (int i = a; i < b; i++)
+	cout << "Enter the diapazone:" << endl;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+
+	for (int i = 0; i < n; i++)
 	{
-		mass[i] = (rand() % 10 - 10);
-		cout << mass[i] << "\n";
-		count++;
+		mass[i] = rand() % (b - a + 1) + a;
+		cout << mass[i] << " ";
 	}
+
+	cout << endl;
 	cout << "k = ";
 	cin >> k;
-	for (int i = a; i < b; i++)
+
+	for (int i = 0; i < n; i++)
 	{
-		mass[i] = mass[i] * 10 + (-k);
-		cout << mass[i] << "\n";
+		if (mass[i] < 0)
+		{
+			int z = k;
+			c = 0;
+			while (z > 0)
+			{
+				z = z / 10;
+				c++;
+			}
+			while (c != 0)
+			{
+				mass[i] = mass[i] * 10;
+				c=c-1;
+			}
+			mass[i] = mass[i] - abs(k);
+		}
+		cout << mass[i] << " ";
 	}
 	return 0;
 }
