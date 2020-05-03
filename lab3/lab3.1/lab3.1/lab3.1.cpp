b58dce99db3b1c23d61c65f2a365704401449340
paveltrushkov <56478385+paveltrushkov@users.noreply.github.com>
2020-05-03 13:24:49 +0700

    лабораторная работа 3, вариант 10

---
+++ lab3/lab3.1/lab3.1/lab3.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int c;
+    while ((c = getchar()) != EOF) {
+        if ('a' <= c && c <= 'z') {
+            c += 'A' - 'a';
+        }
+        putchar(c);
+    }
+    return 0;
+}
