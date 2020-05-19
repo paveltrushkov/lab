35db130d790e02354363f17e01fddde5af085e68
paveltrushkov <56478385+paveltrushkov@users.noreply.github.com>
2020-05-19 14:33:21 +0700

    4 лаба 10 вариант

---
+++ lab4/lab4.3/lab4.3/lab4.3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long int F(int n);
+long long int G(int n);
+
+int main()
+{
+    cout << "F(1) =  " << F(1) << "; G(1) = " << G(1) << endl;
+    for (int i = 2; i <= 10; i++)
+    {
+        cout << "F(" << i << ") =  " << F(i) << "; G(" << i << ") = " << G(i) << endl;
+    }
+
+    return 0;
+}
+
+long long int F(int n) {
+    return n == 1 ? 2 : 2 * F(n - 1) * G(n - 1);
+}
+
+long long int G(int n) {
+    return n == 1 ? 1 : 4 * F(n - 1) + G(n - 1);
+}
