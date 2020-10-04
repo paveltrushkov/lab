3f91dede7c0dadbd0f3807ebe05349769dce71a0
paveltrushkov <56478385+paveltrushkov@users.noreply.github.com>
2020-10-04 21:00:35 +0700

    1 лаба 3 семестр

---
+++ 2lab1/2lab1/2lab1.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+
+using namespace std;
+
+struct complex
+{
+        double p, q;
+};
+
+void сplus(complex a, complex b)
+{
+    cout << "Сумма :" << a.p + b.p << " " << a.q + b.q << endl;
+}
+
+void cmultiplication(complex a, complex b)
+{
+    cout << "Произведение :" << a.p * b.p << " " << a.q * b.q;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    complex a, b;
+    cout << "Complex a: ";
+    cin >> a.p >> a.q;
+    cout << "Complex b: ";
+    cin >> b.p >> b.q;
+    сplus(a, b);
+    cmultiplication(a, b);
+    return 0;
+
+}
