4df0d2c9f0ea1370897ce2eb4c09477b06906336
paveltrushkov <56478385+paveltrushkov@users.noreply.github.com>
2021-02-09 12:26:56 +0800

    Add files via upload

---
+++ javav.java
@@ -0,0 +1,24 @@
+class Example {
+	public static void main(String args[]) {
+		
+	}
+}
+public class Figure
+{
+	public double CalcArea();
+	public void Show();
+}
+
+public class Square extends Figure 
+{
+	private double a;
+	public double CalcArea ()
+	{
+		return a*a;
+	}
+	public void Show();
+	{
+		
+	}
+
+}
